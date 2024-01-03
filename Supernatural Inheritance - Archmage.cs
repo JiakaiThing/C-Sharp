ea27d1cb92edc5af870ee9a8ea75dc9cde137705
Jiakai Thing <106014402+JiakaiThing@users.noreply.github.com>
2024-01-03 22:54:02 +1100

    Supernatural Inheritance - Archmage.cs

---
+++ Supernatural Inheritance - Archmage.cs
@@ -0,0 +1,33 @@
+// Archmage.cs
+using System;
+
+namespace MagicalInheritance
+{
+  //inheriting Title property and CastRainStorm method from Mage class
+  class Archmage : Mage
+  {
+    public Archmage(string title) : base(title)
+    {
+
+    }
+
+    public Archmage(string title, string origin) : base(title, origin)
+    {
+      
+    }
+
+    public override Storm CastRainStorm()
+    {
+      return new Storm("rain", true, Title);
+    }
+
+    public Storm CastLightningStorm()
+    {
+      return new Storm("lightning", true, Title);
+    }
+
+
+    
+  }
+ 
+}

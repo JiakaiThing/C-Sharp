dc1593515f6401fe1d7904d1c4e7a4921eed81ce
Jiakai Thing <106014402+JiakaiThing@users.noreply.github.com>
2024-01-03 21:32:02 +1100

    App Interfaces - IDisplayable.cs

---
+++ App Interfaces - IDisplayable.cs
@@ -0,0 +1,14 @@
+// Define IDisplayable in this file
+
+using System;
+
+namespace SavingInterface
+{
+  interface IDisplayable
+  {
+    void Display();
+    string HeaderSymbol
+    { get; }
+  }
+
+}
